cdc8a4cab8cf0263d8d8defa915254b26e9e3a95
Chance <zzhchance@163.com>
2018-05-21 21:02:23 +0800

    相关参数设置

--- Settings.py
+++ Settings.py
@@ -6,9 +6,9 @@ Created on 2017年12月20日
 '''
 
 # 子任务的关联关系：并行，串行，或
-concurrence = 0     # 并行
+concurrence  = 0        # 并行
 sequentially = 1        # 串行
-Or = 2      # 或
+Or           = 2        # 或
 
 
 # 任务最大层数
